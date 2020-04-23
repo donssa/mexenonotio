a3026a79ccd5fb7162d6b9fe8ca04abc6ca12025
donssa <tosugos@gmail.com>
2020-04-23 08:59:33 -0300

    vo dormi, trata disso depois

--- DONSA.py
+++ DONSA.py
@@ -1,33 +1,38 @@
 #código do git:
 
 from notion.client import NotionClient
+from notion.block import PageBlock
+from notion.block import TodoBlock
+
+import pandas as pd
 
 # Obtain the `token_v2` value by inspecting your browser cookies on a logged-in session on Notion.so
 token="8c367e647c7f3db99590cf5c6a12eaf148c13c825c88134327355f1f1838f54c7d7e5e9636a7b65bf6b91b09fc1e6958fde77bed6a0bb2cb721eb61e0c5984fe4d82e92fa48ef4ae0a706976755e"
 #variável client é o "cliente" aberto no login do navegador. qual a segurança disso? posso tentar testar
 client = NotionClient(token_v2=token)
 
+#O DONSA
+DONSA = client.get_block("29af322f-3eb3-40a7-89be-2dd7afd78408")
 #Todas as páginas do DONSA
 HOMEPAGE = client.get_block("29cd1701-b3f6-495e-b94e-535a2c03be37")
+#sandbox pro python
+sandbox = client.get_block("ab9ebc44-60fb-4dd6-8bb0-f4ca08d9730e")
 Calendário = client.get_block("125a4883-97b2-4181-a46c-e0e42f40cd91")
 
-kidshome=HOMEPAGE.children
-print(Calendário)
-titulo=HOMEPAGE.title
-pais=HOMEPAGE.parent
+coluna = client.get_collection("058cd2b2-735c-45a6-bc75-6be2b59caaa1")
 
-for child in HOMEPAGE.children:
-    print(child)
+filter_params = [{"TIPO": "DataCamp"}]
 
-print("Parent of {} is {}".format(HOMEPAGE.id, HOMEPAGE.parent.id))
+#print(client.get_block("125a4883-97b2-4181-a46c-e0e42f40cd91"))
 
-#print("PARENTS:",pais,'\n')
-#print("O tipo de estrutura do título é:",type(titulo),'\n')
-#print("The child titles are:",'\n',kidshome,'\n')
-#print("O tipo de estrutura dos blocos é:",type(kidshome),'\n')
+#print(sandbox.children[1])
 
-# Note: You can use Markdown! We convert on-the-fly to Notion's internal formatted text data structure.
+#sandbox.children.add_new(PageBlock, title='foi meu copaero')
+
+#sandbox.children[0].add_new(PageBlock, title='foi meu copaero')
 
 
+#for child in HOMEPAGE.children:
+#    print(child)
 
 #input("ENTER EXIT")
---
+++ README.txt
@@ -0,0 +1,5 @@
+CONSEGUI CONECTAR NO NOTION CARALHO
+
+ok agora preciso pensar em o que otimizar
+
+me parece uma boa ideia criar uma checklist diária nova
